693f95826bf9e03026f15cb8a505a4b0dcc1bda6
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-29 02:27:31 +0530

    Create Simple Linked List and Add First

---
+++ CustomLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListProblem
+{
+    class CustomLinkedList
+    {
+        public Node head;        
+        public void AddFirst(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine("Linked is Emptyp");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace LinkedListProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To liked List Programs!");
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddFirst(56);
+            customLinkedList.AddFirst(30);
+            customLinkedList.AddFirst(70);
+            customLinkedList.Display();
         }
     }
 }
